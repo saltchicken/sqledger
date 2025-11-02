ac28df54a79dc4d4e2b8aa992136ddc100435e5f
saltchicken <john.eicher89@gmail.com>
2025-11-01 19:17:05 -0700

    Fix chunk percentages

--- src/ui.rs
+++ src/ui.rs
@@ -11,7 +11,7 @@ use std::{ffi::OsStr, path::Path};
 pub fn ui(f: &mut Frame, app: &mut App) {
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
         .split(f.area());
 
     // --- Left Pane: SQL File List ---
